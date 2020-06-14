03568908ac7b34b0acbd21a425b4a8bd36450128
khactam94 <khac.tam.94@gmail.com>
2020-06-14 23:17:51 +0700

    feat: crawl data from listen a minute

---
+++ index.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Fs = require('fs');
+const Path = require('path')
+
+const url = "https://listenaminute.com";
+
+const downloadFile = async (path) => {
+  try {
+    const subUrl = `${url}/${path[0]}/${path}`;
+    const res = await axios({
+      url: subUrl,
+      method: 'GET',
+      responseType: 'stream'
+    });
+    return res.data.pipe(Fs.createWriteStream(
+      Path.resolve(__dirname, 'data', path)
+    ))
+  } catch (err) {
+    console.log('Failed: ', path);
+  }
+}
+downloadFile('refugees.mp3');
+
+const getFileNameFromLink = async (link) => {
+  try {
+    const res = await axios.get(`${url}/${link}`);
+    const html = res.data;
+    const $ = cheerio.load(html);
+    const links = $('div.audio > audio > source');
+
+    return $(links[0]).attr('src');
+  } catch (err) {
+    console.log('Failed: ', `${url}/${link}`);
+  }
+}
+
+// axios.get(url).then(async (res) => {
+//   const html = res.data;
+//   const $ = cheerio.load(html);
+//   const links = $('table > tbody > tr a');
+//   const linkTails = [];
+//   links.each(function () {
+//     const text = $(this).text();
+//     linkTails.push($(this).attr('href'));
+//   });
+//   let i = 0;
+//   while (linkTails.length) {
+//     await Promise.all(linkTails.splice(20).map(async link => {
+//       const path = await getFileNameFromLink(link);
+//       if (path) {
+//         await downloadFile(path)
+//       }
+//     }));
+//   }
+// }).catch(err => console.log(err))
